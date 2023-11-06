7530c532ec8f11ec5a341937b38b95a5eb126349
Andrzej Gębala <agebala@neonet.pl>
2023-11-06 11:01:43 +0100

    creating initial day view

--- src/components/CurrentDayLesson.js
+++ src/components/CurrentDayLesson.js
@@ -10,29 +10,53 @@ const CurrentDayLesson = ({ lessons, loading }) => {
 
     const { date } = useDate()
     console.log(date);
-    lessons.map((lesson, i) =>{
-console.log(i +1);
-    })
 
     const [selecedDay, setSelectedDay] = useState('')
+    const [checkDate, setCheckDate] = useState(true)
 
-    useEffect((day) => {
+    const buttonHandler = (day) =>{
         setSelectedDay(day)
-    }, [])
+        setCheckDate(false)
+    }
+    // useEffect(() => {
+    //     buttonHandler()
+    // }, [])
 
     return (
         <div className="current-day">
-            
+
             <div className="dayname-btn-container">
                 {lessons && lessons.map((lesson) => (
-                    <Button onClick={() => setSelectedDay(lesson.day)} value={lesson.day} key={lesson.id} />
+                    <Button onClick={() => buttonHandler(lesson.day)} value={lesson.day} key={lesson.id} />
                 ))}
             </div>
             {loading && <Loading />}
 
             {lessons.map((lesson, i) => (
 
-                
+               checkDate && lesson.day.trim() === date &&
+              
+                <div key={i}>
+
+                    <div className="current-timetable" key={lesson.id}>
+                        <p className="day">{lesson.day}</p>
+                        <p className="dinner-time">obiad: {lesson.dinner}</p>
+                    </div>
+
+                    <div className="current-day-plan">
+                        {lesson.hours.map((hour, i) => (
+                            <div className="current-day-plan__map" key={i}>
+                                <p className="hour">{hour}</p>
+                                <p className="lesson">{lesson.lessons[i]}</p>
+                            </div>
+                        ))}
+                    </div>
+
+                </div>
+            ))}
+
+            {lessons.map((lesson, i) => (
+
                 lesson.day === selecedDay &&
                 <div key={i}>
 
