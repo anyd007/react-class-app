34ba5a2c1170833f43b0e10307bb0bc2807cfe7b
Andrzej <andrzej.gebala35@gmail.com>
2023-11-30 10:40:43 +0100

    create edit item for notes

--- src/user/Notes.js
+++ src/user/Notes.js
@@ -7,16 +7,19 @@ import AddNotepopup from "../ui/AddNotePopuop";
 import { useNavContext } from '../components/NavContext';
 import useFirebase from "../config/useFirebase";
 import Loading from "../ui/Loading";
-import { animated, useSpring } from '@react-spring/web'
+
 import '../styles/notes.scss';
 
 const Notes = () => {
     const navigate = useNavigate()
     const { openPopup, setOpenPopup } = useNavContext()
     const { notes, loading } = useFirebase()
-    const [edit, setEdit] = useState(false)
-    const [editItem, setEditItem] = useState('')
-    const [isInputVisible, setInputVisible] = useState(false);
+    const [editItem, setEditItem] = useState({
+        id: '',
+        title: '',
+        txt: ''
+    })
+
 
 
     const handleBack = () => {
@@ -26,21 +29,24 @@ const Notes = () => {
         setOpenPopup(true)
     }
 
-    const props = useSpring({
-        from: { fontSize: "0px" },
-        to: { fontSize: isInputVisible ? "16px" : "0px" },
-      });
 
-    const handleOpenEdit = (id) => {
 
-        setEdit(true)
-        setEditItem(id)
-        if(editItem !==id){
-        // setInputVisible(!isInputVisible)
-        }
+    const handleOpenEditTitle = (id, title) => {
+        setEditItem({
+            id,
+            title
+        })
+
+    }
+    const handleOpenEditTxt = (id, txt) => {
+        setEditItem({
+            id,
+            txt
+        })
+
     }
 
-   
+    console.log(editItem);
 
     return (
         <div className="notes">
@@ -53,19 +59,14 @@ const Notes = () => {
                 <FaBackspace className='back' onClick={handleBack} />
                 <button onClick={handleopenPopup}>dodaj przypomnienie</button>
                 {notes && notes.map((note, index) => (
-                <div className="notes-items" key={index}>
-                    <h3>{note.noteTitle}</h3>
-                    {edit && editItem === note.id ?
-                        <animated.input 
-                        type="text" 
-                        maxLength="50"
-                        style={{ fontSize: props.fontSize }}
-                         />
-                        : null}
-                    <p>{note.noteTxt}</p>
-                    <MdDoneOutline className="done" />
-                    <FaEdit className="edit" onClick={() => handleOpenEdit(note.id)} />
-                </div>
+                    <div className="notes-items" key={index}>
+                        <FaEdit className="edit-title" onClick={() => handleOpenEditTitle(note.id, note.noteTitle)} />
+                        <h3>{note.noteTitle}</h3>
+                        <p>{note.noteTxt}</p>
+                        <FaEdit className="edit-txt" onClick={() => handleOpenEditTxt(note.id, note.noteTxt)} />
+                        <MdDoneOutline className="done" />
+
+                    </div>
                 ))}
             </div>
         </div>
