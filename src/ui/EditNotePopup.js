9abc7a9b36c1864f5eecd699b6ed24458d32f046
Andrzej <andrzej.gebala35@gmail.com>
2023-11-30 13:24:42 +0100

    add updateDoc function for note text

--- src/ui/EditNotePopup.js
+++ src/ui/EditNotePopup.js
@@ -1,10 +1,14 @@
 import { doc, updateDoc } from 'firebase/firestore';
+import useFirebase from '../config/useFirebase';
 import '../styles/edit-note-popup.scss';
 import { useState } from 'react';
 import { db, auth } from '../config/firebase';
+import Loading from './Loading';
 
 const EditNotePopup = ({ setOpenEditPopup, editItem }) => {
     const [editTitle, setEditTitle] = useState('')
+    const [editTxt, setEditTxt] = useState('')
+    const {loading} = useFirebase();
 
     const handleClousePopup = () => {
         setOpenEditPopup(false)
@@ -13,9 +17,13 @@ const EditNotePopup = ({ setOpenEditPopup, editItem }) => {
         try {
             const userId = auth?.currentUser?.uid;
             const noteTitle = doc(db, "users", userId, "notes", editItem.id)
-            if(editItem.title){
-            await updateDoc(noteTitle, {noteTitle: editTitle})
-            setOpenEditPopup(false)
+            if (editItem.title) {
+                await updateDoc(noteTitle, { noteTitle: editTitle })
+                setOpenEditPopup(false)
+            }
+            else if (editItem.txt) {
+                await updateDoc(noteTitle, { noteTxt: editTxt })
+                setOpenEditPopup(false)
             }
         } catch (err) {
             console.error('Błąd podczas zmiany daych:', err.message);
@@ -24,7 +32,8 @@ const EditNotePopup = ({ setOpenEditPopup, editItem }) => {
 
     return (
         <div className="edit-note-popup">
-            <button onClick={handleClousePopup} className='clouse-btn'>zamknij</button>
+            {loading && <Loading/>}
+         
             <div className="edit-note-popup__container">
                 <div className="edit-note-popup__container__header">
                     <h2>Edytuj przypomnienie</h2>
@@ -40,13 +49,22 @@ const EditNotePopup = ({ setOpenEditPopup, editItem }) => {
                             placeholder='podaj nowy tytuł....'
                             value={editTitle}
                             onChange={(e) => setEditTitle(e.target.value)}
-                        />
+                        /><br/>
                         <button onClick={handleApplyEdit}>zatwierdź</button>
                         <button onClick={handleClousePopup}>odrzuć</button>
                     </div> : null}
                     {editItem.txt ? <div>
                         <h3>dotychczasowy tekst przypomnienia:</h3>
                         <p>{editItem.txt}</p>
+                        <h3>nowy tekst przypomnienia:</h3>
+                        <textarea
+                            maxLength="200"
+                            placeholder='podaj nowy tekst....'
+                            value={editTxt}
+                            onChange={(e) => setEditTxt(e.target.value)}
+                        ></textarea><br/>
+                        <button onClick={handleApplyEdit}>zatwierdź</button>
+                        <button onClick={handleClousePopup}>odrzuć</button>
                     </div> : null}
                 </div>
             </div>
